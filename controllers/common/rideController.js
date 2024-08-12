55be6e8ac880d9372a43a731d29b1206c2f354a4
Sphaeram <Sameer.Cs954@gmail.com>
2024-08-12 23:16:51 +0500

    car id drop points issue fix

--- controllers/common/rideController.js
+++ controllers/common/rideController.js
@@ -2,7 +2,8 @@ const db = require("../../models");
 
 const getPickupPoints = async (req, res) => {
   const { carId } = req.query;
-  if (!carId || isNaN(carId)) return res.status(400).json({ data: "Bad Request!" });
+  if (!carId || isNaN(carId))
+    return res.status(400).json({ data: "Bad Request!" });
 
   try {
     const airports = await db.airport_fare.findAll({
@@ -30,24 +31,30 @@ const getPickupPoints = async (req, res) => {
 };
 
 const getDropPoints = async (req, res) => {
-  const { pickupPoint } = req.query;
+  const { pickupPoint, carId } = req.query;
   if (!pickupPoint) return res.status(400).json({ data: "Bad Request!" });
 
   try {
     const airports = await db.airport_fare.findAll({
       attributes: ["drop_location", "fare"],
-      where: { pickup_location: pickupPoint },
+      where: { pickup_location: pickupPoint, car_id: carId },
       raw: true,
     });
     const railways = await db.railway_fare.findAll({
       attributes: ["drop_location", "fare"],
-      where: { pickup_location: pickupPoint },
+      where: { pickup_location: pickupPoint, car_id: carId },
       raw: true,
     });
 
     const dropPoints = [
-      ...airports.map((airport) => ({ drop_point: airport.drop_location, fare: airport.fare })),
-      ...railways.map((railway) => ({ drop_point: railway.drop_location, fare: railway.fare })),
+      ...airports.map((airport) => ({
+        drop_point: airport.drop_location,
+        fare: airport.fare,
+      })),
+      ...railways.map((railway) => ({
+        drop_point: railway.drop_location,
+        fare: railway.fare,
+      })),
     ];
 
     return res.status(200).json({ data: dropPoints });

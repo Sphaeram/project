2b14bdf60873e7bb7e2a9b7a87071e7f29f74b0f
Sphaeram <Sameer.Cs954@gmail.com>
2024-08-12 22:45:08 +0500

    set users to hard delete

--- controllers/admin/userController.js
+++ controllers/admin/userController.js
@@ -59,7 +59,7 @@ module.exports = {
       if (!user) return res.status(404).json({ data: "User Not Found!" });
       if (user.user_type_id === 6156) return res.sendStatus(403);
       if (req.user.id === parseInt(userId) || req.user.user_type_id === 6156) {
-        await db.user.destroy({ where: { id: user.id } });
+        await db.user.destroy({ where: { id: user.id }, force: true });
         return res.status(200).json({ data: "User Deleted!" });
       } else return res.sendStatus(403);
     } catch (error) {
@@ -72,7 +72,9 @@ module.exports = {
       const users = await db.user.findAll();
       if (users.length === 0)
         return res.status(404).json({ data: "No Users Found!" });
-      await db.user.destroy({ where: { user_type_id: { [Op.not]: 6156 } } });
+      await db.user.destroy({
+        where: { user_type_id: { [Op.not]: 6156 }, force: true },
+      });
       return res.status(200).json({ data: "All Users Deleted!" });
     } catch (error) {
       return res.status(500).json({ data: error.message });

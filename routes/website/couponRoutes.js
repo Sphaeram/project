405455d6c81042fec6b115dec4aeea17730c184b
Sphaeram <Sameer.Cs954@gmail.com>
2024-08-18 22:03:59 +0500

    fix

--- routes/website/couponRoutes.js
+++ routes/website/couponRoutes.js
@@ -4,7 +4,7 @@ const { verifyLogin } = require("../../middlewares/verify");
 
 const router = require("express").Router();
 
-router.post("/check-coupon", verifyLogin, couponController.checkCouponByCode);
+// router.post("/check-coupon", verifyLogin, couponController.checkCouponByCode);
 
 router.get("/check-coupon", verifyCoupon);
 
